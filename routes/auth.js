1eac39dc081637a3ec529de90c7a34fa2632cb00
Melveta Atkinson <melvetaatkinson818@gmail.com>
2020-03-24 20:25:42 -0400

    Correcting emitted error on function issue

--- routes/auth.js
+++ routes/auth.js
@@ -5,20 +5,20 @@ const jwt = require ('jsonwebtoken');
 
 //Signup
 authRouter.post('/signup', (req, res, next) => {
-    User.findOne({username: req.body.username.toLowerCase()}, (err, existingUser) => {
-        if (err) {
-            res.status(500);
-            return next(err);
-        }
-        if (exisingUser !== null) {
-            res.status(400);
-            return next(new Error("That username already exists"));
-        }
+    User.findOne({username: req.body.username.toLowerCase()}, (error, existingUser) => {
+        // if (error) {
+        //     res.sendStatus(500);
+        //     return next(error);
+        // }
+        // if (exisingUser !== null) {
+        //     res.status(400);
+        //     return next(new Error("That username already exists"));
+        // }
         const newUser = new User (req.body);
-        newUser.save((err, user) => {
-            if (err) {
-                res.status(500);
-                return next(err);
+        newUser.save((error, user) => {
+            if (error) {
+                res.sendStatus(500);
+                return next(error);
             }
             const token = jwt.sign(user.toObject(), process.env.SECRET);
             return res.status(201).send({success: true, user: user.toObject(), token});
@@ -28,9 +28,9 @@ authRouter.post('/signup', (req, res, next) => {
 
 //Login
 authRouter.post('/login', (req, res, next) => {
-    User.findOne({username: req.body.username.toLowerCase()}, (err, user) => {
-        if (err) {
-            return next(err);
+    User.findOne({username: req.body.username.toLowerCase()}, (error, user) => {
+        if (error) {
+            return next(error);
         }
         if (!user || user.password !== req.body.password) {
             res.status(403);
