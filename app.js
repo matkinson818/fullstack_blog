636d8e2223fd30601f333917bf130166484dbde0
Melveta Atkinson <melvetaatkinson818@gmail.com>
2020-04-05 23:44:35 -0400

    Fixing more error with files and directories

--- app.js
+++ app.js
@@ -4,40 +4,42 @@ const authRouter = require('./routes/auth');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const errorHandler = require('./middleware/errorHandler');
 
 //Load env vars
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+// Connecting to the MongoDB database
+mongoose.connect(process.env.CONN_URI, 
+    { useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false, 
+      'useCreateIndex': true
+    });
+
+mongoose.Promise = global.Promise;
 
 // Setup express app
 const app = express();
+
+//Body Parser
+app.use(express.json());
 
 // Use logger middleware
 if(process.env.NODE_ENV == 'development') {
     app.use(morgan('tiny'));
 };
 
-
-// Connecting to the MongoDB database
-mongoose.connect(process.env.CONN_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, 'useCreateIndex': true });
-mongoose.Promise = global.Promise;
-
-// Setting up a port variable
-const PORT = process.env.PORT || 5000;
-
-// Reading the body and parsing it into json
-app.use(bodyParser.json());
-
-// error handling middleware
-app.use((err, req, res, next) => {
-    res.status(422).send({error: err.message});
-})
-
 //Initalize routes
 app.use('/api/v1/blogs', blogs);
 
-app.use("/auth", require("./routes/auth"));
+app.use(errorHandler);
+
+// app.use("/auth", require("./routes/auth"));
+
+// Setting up a port variable
+const PORT = process.env.PORT || 5000;
 
 // Listen for requests
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
